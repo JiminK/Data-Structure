c198fb25bde09f836ec455a0b1bb32f2aac10b6a
짐니 <jimin981005@gmail.com>
2021-05-25 04:29:47 +0900

    Add 20181597.java to lab06

---
+++ lab/20181597.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+/**
+
+ */
+
+class ArrayTree {
+	int[] tree; // tree
+	int largestNum; // the largest node number in the binary tree
+	int capacity; // size of the tree tree
+	
+	ArrayTree(int theCapacity) { // ArrayTree constructor.
+		capacity = theCapacity;
+		tree = new int[capacity]; // tree[0] is not used
+		largestNum = 0;
+	}
+
+	public String toString() { // Show all the element in sequence
+		String str = new String();
+		str = "Tree : - ";
+
+		// print all the nodes in the tree
+		for (int i = 1; i <= largestNum; i++)
+			str += tree[i] + "  ";
+		return str;
+	}
+
+	void Init(int[] es, int n) {
+		// initialize the tree by using the input
+		largestNum = n;
+		for (int i = 1; i <= n; i++)
+			tree[i] = es[i];
+	}
+
+	void PreOrder(int node) {
+		if (node > largestNum)
+			return;
+
+		// NEED TO IMPLEMENT
+		else if (node != 0 && tree[node] != 0) {
+			System.out.print(tree[node] + " ");
+			PreOrder(node * 2);
+			PreOrder(node * 2 + 1);
+
+		}
+
+	}
+
+	void InOrder(int node) {
+		if (node > largestNum)
+			return;
+
+		// NEED TO IMPLEMENT
+		else if (node != 0 && tree[node] != 0) {
+			
+			InOrder(node * 2);
+			System.out.print(tree[node] + " ");
+			InOrder(node * 2 + 1);
+		}
+
+	}
+
+	void PostOrder(int node) {
+		if (node > largestNum)
+			return;
+
+		// NEED TO IMPLEMENT
+		else if (node != 0 && tree[node] != 0) {
+			PostOrder(node * 2);
+			PostOrder(node * 2 + 1);
+			System.out.print(tree[node] + " ");
+
+		}
+
+	}
+
+}
+
+
